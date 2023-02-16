16fe2f0b30ff0efe485b73354b5ad5b4a6f1a9dc
lorenzo <ld6@njit.edu>
2023-02-15 21:40:46 -0500

    Initial commit

---
+++ app/src/main/java/com/codepath/bestsellerlistapp/BestSellerBook.kt
@@ -0,0 +1,32 @@
+package com.codepath.bestsellerlistapp
+
+import com.google.gson.annotations.SerializedName
+
+/**
+ * The Model for storing a single book from the NY Times API
+ *
+ * SerializedName tags MUST match the JSON response for the
+ * object to correctly parse with the gson library.
+ */
+class BestSellerBook {
+    @SerializedName("rank")
+    var rank = 0
+
+    @JvmField
+    @SerializedName("title")
+    var title: String? = null
+
+    @JvmField
+    @SerializedName("author")
+    var author: String? = null
+
+    //TODO bookImageUrl
+    @SerializedName("poster_path")
+    var bookImageUrl: String? = null
+
+    //TODO description
+    @SerializedName("overview")
+    var description: String? = null
+
+    //TODO-STRETCH-GOALS amazonUrl
+}
